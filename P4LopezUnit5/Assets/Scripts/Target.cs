d6a548edf4ed4d086aa7e623141d3e61c6cdaa8c
729378-PL <729378@student.dusd.net>
2026-05-29 11:46:13 -0700

    done
    done

--- P4LopezUnit5/Assets/Scripts/Target.cs
+++ P4LopezUnit5/Assets/Scripts/Target.cs
@@ -11,6 +11,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    public ParticleSystem explosionParticle;
     public int pointValue;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,6 +24,7 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+        
     }
 
     // Update is called once per frame
@@ -33,13 +35,23 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
     }
 
     Vector3 RandomForce()
